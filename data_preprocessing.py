13c1b26d21d61c70098628ba0024509b78edfa0b
1nNoVAt10N <122664787+1nNoVAt10N@users.noreply.github.com>
2025-03-07 14:58:27 +0800

    Add files via upload

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -3,101 +3,41 @@ import cv2
 import numpy as np
 import tqdm
 import matplotlib.pyplot as plt
+from ietk import methods
+from ietk import util
+from ietk.data import IDRiD
 #one-hot表示多标签
-import numpy as np
 import os
-import time
-from PIL import Image
-import cv2
-import numpy as n
 # OpenCV实现MSR
+def normalize_to_01(arr):
+ 
+    arr = arr.astype(np.float64)
+    # 计算数组的最小值和最大值
+    min_val = np.min(arr)
+    max_val = np.max(arr)
+    if min_val == max_val:
+        normalized_arr = np.zeros(arr.shape, dtype=np.float64)
+    else:
+        normalized_arr = (arr - min_val) / (max_val - min_val)
+ 
+    return normalized_arr
 
 
-def fov_extraction(img, image_name):
-        # Read the image
+def map_to_0_255(original_array):
+    # 找到数组中的最小值和最大值
+    min_value = np.min(original_array)
+    max_value = np.max(original_array)
+ 
+    # 检查最大值和最小值是否相同，避免除以零
+    if max_value == min_value:
+        raise ValueError("所有数值都相同，无法进行映射")
+ 
+    # 初始化映射后的数组
+    # mapped_array = []
+ 
+    mapped_array = 255 * ((original_array - min_value) / (max_value - min_value))
 
-        # RGB -> BGR
-        # Extract the red channel
-        red_channel = img[:,:,2]
-
-        # Compute the centerlines
-        h, w = red_channel.shape
-        Hcenterline = h // 2
-        Vcenterline = w // 2
-        print(Hcenterline)
-        # Extract intensity profiles along the centerlines
-        horizontal_profile = red_channel[Hcenterline, :]
-        vertical_profile = red_channel[:, Vcenterline]
-        
-        # Compute threshold from the horizontal profile
-        Hthreshold = np.max(horizontal_profile) * 0.06
-
-        # Compute threshold from the horizontal profile
-        Vthreshold = np.max(vertical_profile) * 0.06
-        
-        # Identify transitions based on the threshold
-       
-        binary_horizontal_profile = (horizontal_profile > Hthreshold).astype(int)
-        binary_vertical_profile = (vertical_profile > Vthreshold).astype(int)
-
-        diff_horizontal = np.diff(binary_horizontal_profile)
-        diff_vertical = np.diff(binary_vertical_profile)
-
-        transitions_horizontal = np.where(diff_horizontal != 0)[0]
-        transitions_vertical = np.where(diff_vertical != 0)[0]
-
-        
-        # fix for no vertical / horizontal transition because of too much zoom 
-        # we use original width / height of image
-        if len(transitions_horizontal) < 2:
-            print("Could not find horizontal transitions for", "img")
-            print("Using original width of image so no cropping applied")
-            transitions_horizontal = [0, img.shape[1]-1]
-        
-        if len(transitions_vertical) < 2:
-            print("Could not find vertical transitions for", "img")
-            print("Using original height of image so no cropping applied")
-            transitions_vertical = [0, img.shape[0]-1]
-        
-        # use maximum and minimum transition because more than 2 transitions can be found
-        vertical_diff = transitions_vertical[-1] - transitions_vertical[0]
-        horizontal_diff = transitions_horizontal[-1] - transitions_horizontal[0]
-        
-        # if horizontal_diff < img.shape[1] * 0.25 or horizontal_diff < vertical_diff * 0.9:
-        if horizontal_diff < img.shape[1] * 0.25:
-            print("horizontal diff: ", horizontal_diff, "vertical diff: ", vertical_diff)
-            print('image size: ', img.shape)
-
-            print("Wrong transition detected so no cropping for ", "img")
-            transitions_horizontal = [0, img.shape[1]-1]
-
-        if vertical_diff < img.shape[0] * 0.25:
-            print("horizontal diff: ", horizontal_diff, "vertical diff: ", vertical_diff)
-            print('image size: ', img.shape)
-            print("Wrong vertical transition detected so no cropping for ", "img")
-            transitions_vertical = [0, img.shape[0]-1]
-
-        if len(transitions_vertical) > 2:
-            print("More than 2 vertical transitions for", "img")
-            print(transitions_vertical)
-
-        
-        if len(transitions_horizontal) > 2:
-            print("More than 2 horizontal transitions for", "img")
-            print(transitions_horizontal)
-    
-    
-        
-        # Crop image based on the transitions
-        cropped_img = img[transitions_vertical[0]:transitions_vertical[-1], transitions_horizontal[0]:transitions_horizontal[-1]]
-        # print('full path', full_path)
-        #cv2.imwrite(full_path, cropped_img)
-        # Convert to PIL image for compatibility with torchvision transforms
-
-
-        return cropped_img
-
-
+    return mapped_array
 one_hot = {
     "N":0,
     "D":1,
@@ -108,8 +48,9 @@ one_hot = {
     "M":6,
     "O":7,
 }
+
 class PreprocessAndCache:
-    def __init__(self, img_dir, information_file, cache_dir="./temp_cache"):
+    def __init__(self, img_dir, information_file, cache_dir="./test"):
         self.img_dir = img_dir
         self.information = pd.read_excel(information_file)
         self.cache_dir = cache_dir
@@ -119,13 +60,28 @@ class PreprocessAndCache:
 
     def preprocess_img(self,img_path):
             img = cv2.imread(img_path)
-            
-
             img = cv2.resize(img, (224, 224))
             
             if img is None:
                 return np.ones((384, 384, 3)) * 255  # 这里可以返回一个全白图像作为默认值
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            
+            print(f"Before CLAHE, img min: {img.min()}, max: {img.max()}")
+            lab = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
+            l, a, b = cv2.split(lab)
+            clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+            l = clahe.apply(l)
+            lab = cv2.merge((l, a, b))
+            img = cv2.cvtColor(lab, cv2.COLOR_LAB2RGB)
+            # img = normalize_to_01(img)
+            # I = img.copy()
+            # I, fg = util.center_crop_and_get_foreground_mask(I)
+            # enhanced_img = methods.brighten_darken(I, 'A+B+C+X', focus_region=fg)
+            # # enhanced_img = (enhanced_img-enhanced_img.min())/(enhanced_img.max()-enhanced_img.min())
+            # enhanced_img2 = methods.sharpen(enhanced_img, bg=~fg)
+            # enhanced_img2 = cv2.resize(enhanced_img2, (256, 256))
+            
+            
             image_blur = cv2.GaussianBlur(img, (63, 63), sigmaX=10, sigmaY=10)
             
             # 转换为浮点类型以进行数学运算
@@ -163,7 +119,7 @@ class PreprocessAndCache:
                     else:
                         label.append(0)
                 label = np.array(label)
-                np.savez_compressed(cache_path, img=img, label=label)
+                np.savez_compressed(cache_path, img=img, label=label,name = row["Left-Fundus"])
 
     def __getitem__(self, index):
         alldata = self.information.iterrows()
@@ -185,24 +141,33 @@ class PreprocessAndCache:
         return len(self.information)
     
 class PreprocessAndCache_for_single:
-    def __init__(self,left_img,right_img,cache_dir="./temp_cache"):
+    def __init__(self,left_img=None,right_img=None,cache_dir="./5K",pre=True):
         #这里的左右图片全为路径
         self.left_img = left_img
         self.right_img = right_img
         self.cache_dir = cache_dir
         os.makedirs(cache_dir, exist_ok=True)
         self.one_hot = ["N", "D", "G", "C", "A", "H", "M", "O"]
-        self._preprocess_and_cache()
+        self.pre = pre
+        if self.pre:
+            self._preprocess_and_cache()
 
     def preprocess_img(self,img_path):
             img = cv2.imread(img_path)
-            img = fov_extraction(img,"1")
             img = cv2.resize(img, (224, 224))
-
+            
             if img is None:
                 return np.ones((384, 384, 3)) * 255  # 这里可以返回一个全白图像作为默认值
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
+            
+            print(f"Before CLAHE, img min: {img.min()}, max: {img.max()}")
+            lab = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
+            l, a, b = cv2.split(lab)
+            clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+            l = clahe.apply(l)
+            lab = cv2.merge((l, a, b))
+            img_clahe = cv2.cvtColor(lab, cv2.COLOR_LAB2RGB)
+            
             image_blur = cv2.GaussianBlur(img, (63, 63), sigmaX=10, sigmaY=10)
             
             # 转换为浮点类型以进行数学运算
@@ -217,12 +182,12 @@ class PreprocessAndCache_for_single:
             # 限制像素值范围并转换回uint8类型
             enhanced = np.clip(enhanced, 0, 255).astype(np.uint8)
 
+
             return enhanced
 
 
 
     def merge_double_imgs(self, left_eye_path, right_eye_path):
-        print(left_eye_path)
         left_img = self.preprocess_img(left_eye_path)
         right_img = self.preprocess_img(right_eye_path)
         return np.concatenate([left_img, right_img], axis=-1)
@@ -250,4 +215,5 @@ class PreprocessAndCache_for_single:
 
 if __name__ == "__main__":
     print(one_hot.keys())
-    d1 = PreprocessAndCache_for_single("./hhh/1_left.jpg","./hhh/1_right.jpg")
+    # d1 = PreprocessAndCache("F:\waibao\cropped_5K","F:\Retinal-disease-foundational-model-for-ODIR2019-main\data\ODIR-5K_Training_Annotations(Updated)_V2.xlsx")
+    d1 = PreprocessAndCache("F:\waibao\cropped_#Training_Dataset","F:\waibao\Traning_Dataset.xlsx")
